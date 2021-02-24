9b7f0f9c25ed32db3f3024e4cf937bd6f1b2d061
frank0417 <frank0417@163.com>
2021-02-24 15:29:52 +0800

    add

--- source/src/BinaryTreeLevelOrderTraversal.java
+++ source/src/BinaryTreeLevelOrderTraversal.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinaryTreeLevelOrderTraversal {
     /*
@@ -7,36 +9,49 @@ public class BinaryTreeLevelOrderTraversal {
      * 链 接: https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
      * 描 述：给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
      */
-    public List<List<Integer>> levelOrderTraversal(TreeNode root){
-        List<List<Integer>>res=new ArrayList<>();
-        levelOrder(root,0,res);
-        return  res;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        bfs(0, root, result);
+        return result;
     }
 
-    public void levelOrder(TreeNode root,Integer level,List<List<Integer>>res){
-        if(root==null)return;
-
-        if(res.size()==level){
-            res.add(new ArrayList<Integer>());
-        }
-
-        res.get(level).add(root.val);
-
-        if(root.right!=null){
-            levelOrder(root.right,level+1,res);
-        }
-        if(root.left!=null){
-            levelOrder(root.left,level+1,res);
-        }
+    public void bfs(int level, TreeNode root, List<List<Integer>> result) {
+        if (root == null) return;
+        if (level == result.size()) result.add(new ArrayList<>());
+        result.get(level).add(root.val);
+        bfs(level + 1, root.left, result);
+        bfs(level + 1, root.right, result);
     }
+
+    //非递归实现
+    public List<List<Integer>> levelOrderII(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> dq = new LinkedList<>();
+        dq.add(root);
+        while (!dq.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = dq.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode node = dq.poll();
+                list.add(node.val);
+                if (node.left != null) dq.add(node.left);
+                if (node.right != null) dq.add(node.right);
+            }
+            result.add(list);
+        }
+        return result;
+    }
+
 
     public static void main(String[] args) {
-        BinaryTreeLevelOrderTraversal b=new BinaryTreeLevelOrderTraversal();
-        TreeNode ll2=new TreeNode(31);
-        TreeNode lr2=new TreeNode(32);
-        TreeNode ll1=new TreeNode(21,ll2,lr2);
-        TreeNode lr1=new TreeNode(22);
-        TreeNode root=new TreeNode(1,ll1,lr1);
-        b.levelOrderTraversal(root);
+        BinaryTreeLevelOrderTraversal b = new BinaryTreeLevelOrderTraversal();
+        TreeNode ll2 = new TreeNode(31);
+        TreeNode lr2 = new TreeNode(32);
+        TreeNode ll1 = new TreeNode(21, ll2, lr2);
+        TreeNode lr1 = new TreeNode(22);
+        TreeNode root = new TreeNode(1, ll1, lr1);
+        b.levelOrderII(root);
     }
 }
--- source/src/MinimumGeneticMutation.java
+++ source/src/MinimumGeneticMutation.java
@@ -1,15 +1,14 @@
 import java.util.*;
 
-public class Test {
-
-    public static void main(String[] args) {
-        Test test = new Test();
-        String[] bank = new String[]{"AATTCCGG","AACCTGGG","AACCCCGG","AACCTACC"};
-        int result = test.minMutation("AACCTTGG", "AATTCCGG", bank);
-        System.out.println(result);
-    }
-
-
+public class MinimumGeneticMutation {
+    /*
+     * 题 目：最小基因变化
+     * 链 接: https://leetcode-cn.com/problems/minimum-genetic-mutation
+     * 描 述：一条基因序列由一个带有8个字符的字符串表示，其中每个字符都属于 "A", "C", "G", "T"中的任意一个。
+     *      假设我们要调查一个基因序列的变化。一次基因变化意味着这个基因序列中的一个字符发生了变化。
+     *      现在给定3个参数 — start, end, bank，分别代表起始基因序列，目标基因序列及基因库，请找出能够使起始
+     *      基因序列变化为目标基因序列所需的最少变化次数。如果无法实现目标变化，请返回 -1。
+     */
     public int minMutation(String start, String end, String[] bank) {
         if (bank == null || bank.length == 0) return -1;
         Set<String> set = new HashSet<>(Arrays.asList(bank));
@@ -48,5 +47,11 @@ public class Test {
         return -1;
     }
 
+    public static void main(String[] args) {
+        Test test = new Test();
+        String[] bank = new String[]{"AATTCCGG","AACCTGGG","AACCCCGG","AACCTACC"};
+        int result = test.minMutation("AACCTTGG", "AATTCCGG", bank);
+        System.out.println(result);
+    }
 
 }
